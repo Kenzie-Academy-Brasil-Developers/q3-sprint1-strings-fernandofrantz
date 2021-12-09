167d49c5424dded27378d191ab9087440ef9ad20
fernandofrantz <fernandofrantz12@gmail.com>
2021-12-09 10:15:22 -0300

    restore .

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # Código do dev aqui
 
-from typing import final
-
-
 def standardize_names(name):
     final = ''
     counter = 0
@@ -13,7 +10,6 @@ def standardize_names(name):
         else:
             final = final + space
         counter += 1
-    print(final)
     return final
 
 def standardize_title(title):
@@ -25,36 +21,59 @@ def standardize_title(title):
             final += word.capitalize() + ' '
         else:
             final += word.capitalize()
-    print(final)
+        counter += 1
     return final
 
-
-text = """
-a famosa atriz Constance Rattigan recebe uma encomenda
-desagradável: uma lista com números de telefone de
-pessoas que morreram recentemente. é uma coisa assustadora,
-considerando que os nomes das poucas pessoas vivas presentes
-na lista estão assinalados em vermelho com uma cruz. O da
-própria Constance é um deles.
-"""
 def standardize_text(text):
     final = ''
-    split = text.split('.')
-    for phrase in split:
-        formated = (phrase[1:])
-        final += formated.capitalize() + '. '
-
-    print(final)
+    splited  = text.split(' ')
+    counter = 0
+    for word in splited:
+        if counter == 0:
+            final += word.capitalize() + ' '
+        elif counter <= len(splited)-2:
+            final += word + ' '
+        else:
+            final += word
+        counter += 1
     return final
 
-
-text = "pense num deserto"
 def title_creator(text):
     phrase = standardize_title(text)
-    print('-' * 20 + phrase + '-' * 20)
     return('-' * 20 + phrase + '-' * 20)
 
 
+
+
+
+
+def text_merge(text_of_blog_a, text_of_blog_b):
+    text_a = ''
+    text_b = ''
+    counter_a = 0
+    counter_b = 0
+    splited_a = text_of_blog_a.split()
+    splited_b = text_of_blog_b.split()
+    phrases_a = text_of_blog_a.split('.')
+    phrases_b = text_of_blog_a.split('.')
+
+    for phrase in phrases_a:
+        dots =  phrase.split('.')
+        print(dots)
+        for frase in dots:
+            print(standardize_text(frase))
+            print('-' *30)
+
+    final = ''
+    for word in splited_a:
+        if counter_a == 0:
+            final += word.capitalize() + ' '
+        elif counter_a < len(splited_a)-1:
+            final += word + ' '
+        counter_a += 1
+    
+    
+    return final
 
 text_of_blog_a = """
 na Londres do pós-guerra, a escritora     Juliet tenta encontrar
@@ -71,32 +90,5 @@ delicado.    usando metáforas culinárias, personagens peculiares
 complexa e      bela ao mesmo tempo.
 """
 
-def text_merge(text_of_blog_a, text_of_blog_b):
-    text_a = ''
-    text_b = ''
-    counter_a = 0
-    counter_b = 0
-    splited_a = text_of_blog_a.split()
-    splited_b = text_of_blog_b.split()
-    for space in splited_a:
-        if counter_a < (len(splited_a) -1):
-            text_a = text_a + space + ' '
-        else:
-            text_a = text_a + space
-        counter_a += 1
-    for space in splited_b:
-        if counter_b < (len(splited_b) -1):
-            text_b = text_b + space + ' '
-        else:
-            text_b = text_b + space
-        counter_a += 1
-
-    formated_a = standardize_text(text_a)
-    formated_b = standardize_text(text_b)
-
-    final = formated_a + formated_b
-
-    print(final)
-    return final
-
-text_merge(text_of_blog_a, text_of_blog_b)
+merged_text = text_merge(text_of_blog_a, text_of_blog_b)
+print(merged_text)
